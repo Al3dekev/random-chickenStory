2cff06f9bd2e25e80943e66f2cf41aff629c8813
Al3dekev <quentin1998@me.com>
2018-11-25 21:48:23 +0100

    Début du projet

---
+++ src/main/java/com/github/al3dekev/chickenstory/ChickenstoryApplication.java
@@ -0,0 +1,12 @@
+package com.github.al3dekev.chickenstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenstoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChickenstoryApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/al3dekev/chickenstory/ChickenstoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.al3dekev.chickenstory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChickenstoryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
